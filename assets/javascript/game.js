cdde3d608d226baf27aac78b9a0979a43ad0afda
lgfahys <lindsey@codeasite.com>
2019-02-21 13:46:51 -0500

    added a word character counter that converts the number of characters in a word to blank spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,25 @@
 window.onload = function (e) {
 
     // correct words
-    var words = ["coffee", "avocadotoast", "acaibowls", "icecream"];
+    var words = ["COFFEE", "AVOCADOTOAST", "ACAIBOWL", "ICECREAM"];
+
+    //selects a random word
+    var randomWord = words[Math.floor(Math.random() * words.length)];
+
     // variables to hold wins, letters remaining and letters guessed
     var wins = 0;
-    var currentWord;
+    var blankSpaces = [];
     var guessesRemaining = 15;
     var allLettersGuessed = "";
 
+    //counts the number of letters for the randomWord
+    for (var i = 0; i < randomWord.length; i++) {
+        blankSpaces[i] = "_ ";
+      }
+
     // variable that holds references to places in html
-    var directionsText = document.getElementById("directions-text");
     var winsText = document.getElementById("wins-text");
-    var currentWordText = document.getElementById("currentWord-text");
+    var blankSpacesText = document.getElementById("blankSpaces-text");
     var guessesRemainingText = document.getElementById("guessesRemaining-text");
     var allLettersGuessedText = document.getElementById("allLettersGuessed-text");
 
@@ -19,10 +27,10 @@ window.onload = function (e) {
     document.onkeyup = function (event) {
         //hides comma before first letter guessed
         if (allLettersGuessed === "") {
-            allLettersGuessed = event.key;
+            allLettersGuessed = event.key.toUpperCase();
             //concatenates allLettersGuessed each time a key is pressed
         } else {
-            allLettersGuessed = allLettersGuessed + ", " + event.key;
+            allLettersGuessed = allLettersGuessed + ", " + event.key.toUpperCase();
         }
 
         // makes guesses go down when user presses an incorrect key/letter
@@ -33,24 +41,18 @@ window.onload = function (e) {
         // alerts user that they lost the game if the run out of guesses
         if (guessesRemaining <= 0) {
             alert("Game Over! You ran out of guesses.");
-            alert("The word was " + currentWord);
+            alert("The word was " + randomWord);
             //asks users if they want to start a new game. if yes, reload page
             var reloadPage = confirm("Do you want to start a new game?");
             if(reloadPage)
             {
               window.location.reload();
             }
-            else
-            {
-    
-            }
-        }
-        else {
-
+        
         };
         // displays number of wins, letters guessed and remaining letters
         winsText.textContent = "Wins: " + wins;
-        currentWordText.textContent = "Current Word: " + currentWordText;
+        blankSpacesText.textContent = "Current Word: " + blankSpaces;
         guessesRemainingText.textContent = "Remaining Guesses: " + guessesRemaining;
         allLettersGuessedText.textContent = "Letters Guessed: " + allLettersGuessed;
     }
